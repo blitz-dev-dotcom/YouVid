6250497f669bf55e40959822484683561fe7fdb3
blitz-dev-dotcom <selvamanjula126@gmail.com>
2023-12-24 11:06:35 +0530

    adsense update

--- src/App.jsx
+++ src/App.jsx
@@ -21,32 +21,34 @@ function App() {
       fetchdata(resp);
     }
     else{
-      setNetwork(false);
+      
     }
    }
    const fetchdata = async(a)=>{
     
-    const url = `https://ytstream-download-youtube-videos.p.rapidapi.com/dl?id=${a}`;
-    const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': '6f7b482cd9msh7256223d5c0819ep14ffc3jsnc16bc9ee9667',
-      'X-RapidAPI-Host': 'ytstream-download-youtube-videos.p.rapidapi.com'
-    }
-  };
-  
-  try {
-    const response = await fetch(url, options);
-    const result =  await response.json();
-    const low  =  await result.formats[0].url;
-    const medium = await result.formats[1].url;
-    const high = await result?.formats[2]?.url;
+    const url = `https://youtube-video-and-shorts-downloader.p.rapidapi.com/?url=${a}`;
+	const options = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Key': '6f7b482cd9msh7256223d5c0819ep14ffc3jsnc16bc9ee9667',
+			'X-RapidAPI-Host': 'youtube-video-and-shorts-downloader.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.json();
+    const audio  =  await result?.audio?.url;
+    const medium = await result?.video[0]?.url;
+    const high = await result?.video[1]?.url;
     //const audio = await result.adaptiveFormats[22].url;
-    setLow(low);
+    setLow(audio);
     setMedium(medium);
     setHigh(high);
+    
     setDown(true);
     //setAudio(audio);
+    
     console.log(result);
     
   } catch (error) {
@@ -56,7 +58,7 @@ function App() {
    
   const HandleClick = (e)=>{
     e.preventDefault();
-    youtube_parser(inputref.current.value);
+    fetchdata(inputref.current.value);
     //fetchdata(inputref.current.value);
     inputref.current.value = "";
     setpress(true);
@@ -71,7 +73,7 @@ function App() {
           <div className='innercontpad'>
           <div className='spantag'>{Network ? "" : <span>Youtube Link invalid!!</span>}</div>
           <h1>YouVid</h1>
-          <p>You can't Download Shorts! But You <br/> can Download long videos</p>
+          
           <div className='inputfield'>
           <input 
             className='inputfield1'
